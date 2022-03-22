5d2eac3e56ee95ff900e62f3760f507d66768ca9
ilghar_kus <ilghar_kus@protonmail.com>
2022-03-22 12:37:36 +0800

    Cleaned garbage, changed themes, wrote a simple publishing script

---
+++ publish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp ./theme ./theme-local.setup ./*.html -r ./public/
+cp ./articles/theme ./articles/style ./articles/theme-local.setup ./articles/style-local.setup ./articles/*.html -r ./public/articles/
+cp ./media -r ./public/
+cp ./articles/media -r ./public/
