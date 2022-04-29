5693e3e9391af16d2c175679b457dab34f8e4b63
ilghar_kus <ilghar_kus@protonmail.com>
2022-04-29 16:50:56 +0800

    Several Pages altered content

--- publish.sh
+++ publish.sh
@@ -3,3 +3,4 @@ cp ./theme ./theme-local.setup ./*.html -r ./public/
 cp ./articles/theme ./articles/style ./articles/theme-local.setup ./articles/style-local.setup ./articles/*.html ./articles/*.pdf ./articles/*.org -r ./public/articles/
 cp ./media -r ./public/
 cp ./articles/media -r ./public/
+rm ./.*~undo-tree~*
