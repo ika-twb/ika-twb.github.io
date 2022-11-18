1b0b3355189cd548f3100d09def78ff127b8667a
ilghar_kus <ilghar_kus@protonmail.com>
2022-11-19 03:25:17 +0800

    Huge update

--- publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp ./*.html -r ./public/
-cp ./articles/*.html ./articles/*.pdf ./articles/*.org -r ./public/articles/
-cp ./media -r ./public/
-cp ./articles/media -r ./public/articles/
